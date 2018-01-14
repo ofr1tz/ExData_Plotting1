bb7d20be28dab83acc848a3967f236fae4b245ef
ofr1tz <ofr1tz@yahoo.de>
2018-01-14 12:16:02 +0000

    transformations code

--- plot1.R
+++ plot1.R
@@ -1,8 +1,26 @@
-# Load archive from the web and unzip
+# Install tidyverse package if necessary
+if (!require(tidyverse)) {
+      install.packages("tidyverse")
+      library(tidyverse)
+}
+
+# Download archive and unzip
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 archive <- tempfile()
 download.file(url, archive)
 unzip(archive, "household_power_consumption.txt")
 
 # Read data from unzipped file
-dat <- read.table("./household/data/household_power_consumption.txt", sep=";", na.strings="?", header=T)
+dat <- as_tibble(read.table("household_power_consumption.txt", sep=";", na.strings="?", header=T))
+
+# Convert date & time
+dat <- dat %>% mutate(Time=dmy_hms(paste(Date, Time))) %>%
+      rename(DateTime = Time) %>%
+      select(-Date)
+
+# Extract cases of 2007-02-01 and 2007-02-02
+dat <- dat %>% filter(DateTime %within% (ymd("2007-02-01") %--% ymd("2007-02-02")))
+
+# Plot
+
+                      
